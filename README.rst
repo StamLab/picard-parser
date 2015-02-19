9ef6af11962c9f636793d34a1c9a8aca08712e6b
mson9 <allswellthatsmaxwell9@gmail.com>
2015-02-19 17:52:05 -0500

    Add README and setup.py

---
+++ README.rst
@@ -0,0 +1,5 @@
+**************************************************
+PicardParser: a tool for processing Picard results
+**************************************************
+
+PicardParser is a tool for automating the processing of Picard result files in Python.
---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+VERSION = "0.1"
+
+DESCRIPTION = """\
+FASTQCParser is a tool for automating the processing of basic FastQC results in Python.
+Given the contents of picard_report.txt, it will parse the results into an object that can
+then be used in code.
+"""
+
+setup(
+    name="picard_parser", 
+    version=VERSION, 
+    description="Parses Picard result files into Python",
+    long_description=DESCRIPTION,
+    url="https://github.com/StamLab/picard-parser",
+    packages=["picard_parser"],
+    license="Python",
+    classifiers=[
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        ],
+    )
